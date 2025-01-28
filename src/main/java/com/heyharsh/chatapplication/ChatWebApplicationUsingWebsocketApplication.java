bbb9f184bf348f0deb2ff271c30db936c8931d2c
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-01-28 19:05:22 +0530

    all done

---
+++ src/main/java/com/heyharsh/chatapplication/ChatWebApplicationUsingWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.heyharsh.chatapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebApplicationUsingWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebApplicationUsingWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heyharsh/chatapplication/ChatWebApplicationUsingWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyharsh.chatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebApplicationUsingWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
